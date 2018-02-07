9ff3227133dd891ab365102202608c45d1f36251
Rico Sonntag <rico.sonntag@netresearch.de>
2018-02-07 09:33:57 +0100

    Add new controller interface

---
+++ module/Application/src/Controller/ControllerInterface.php
@@ -0,0 +1,11 @@
+<?php
+namespace Application\Controller;
+
+interface ControllerInterface
+{
+    const MESSAGE_INFO    = 'info';
+    const MESSAGE_WARN    = 'warning';
+    const MESSAGE_ERROR   = 'danger';
+    const MESSAGE_SUCCESS = 'success';
+
+}
